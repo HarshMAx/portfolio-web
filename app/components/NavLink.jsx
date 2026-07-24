c32e486882f7476a09a9e9b4c1b470be9f17b4ea
harshMAx <harshsolanki4121@gmail.com>
2026-07-24 14:04:44 +0530

    Add new assets: LinkedIn icon SVG and image file

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -1,15 +1,14 @@
-import React from "react";
 import Link from "next/link";
 
 const NavLink = ({ href, title }) => {
+  return (
+    <Link
+      href={href}
+      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+    >
+      {title}
+    </Link>
+  );
+};
 
-    return (
-        <div>
-            <Link href={href} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white
-                        ">{title}</Link>
-        </div>
-    )
-
-}
-
-export default NavLink
+export default NavLink;
